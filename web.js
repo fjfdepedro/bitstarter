a2e15c36144af18b34d5209f986e0c3960b79e69
fjfdepedro <fjfdepedro@gmail.com>
2013-07-10 01:03:36 +0200

    Added index.html

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 
 var app = express.createServer(express.logger());
 
@@ -9,4 +9,10 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+*/
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
