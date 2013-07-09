a8b06e711b4f86b8f8b471e87ef449e2c1934965
fjfdepedro <fjfdepedro@gmail.com>
2013-07-10 01:22:37 +0200

    Added index.html

--- web.js
+++ web.js
@@ -13,8 +13,18 @@ app.listen(port, function() {
 */
 
 
+var express = require('express');
 var fs = require('fs');
-var content = fs.readFileSync("index.html);
-var iconv = new Iconv(encoding, 'UTF-8');
-var buffer = iconv.convert(content);
-buf.toString('utf8');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var buffer = fs.readFileSync('index.html');
+    var out = buffer.toString();
+    response.send(out);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
